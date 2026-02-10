83942c8f5f780bbaf3c54d8092b9a84eb9579edb
PRAGYANSH DIXIT <pragyanshdixit@gmail.com>
2026-02-10 04:31:31 +0000

    10/2/26 -----done

--- Module2/app.js
+++ Module2/app.js
@@ -2,9 +2,37 @@ import express from "express";
 
 const app=express();
 
+const user=[
+    {
+        name:"pragyansh",
+        id:1
+    },
+    {
+        name:"bijay",
+        id:2
+    },
+    {
+        name:"abbhay",
+        id:3
+    }
+]
+
 app.get("/",(req,res)=>{
-    console.log("request is recieve")
+    // console.log("request is recieve")
+    // res.send("<h1> this is the html response </h1>")
+    res.send("this is the text response")
+    // res.send(" two send at a time")        // only one time we can send a response if we do multi response it will give error
+    // res.json({name:"abbhay"})    //this is the json response
 })
+
+app.get("/user",(req,res)=>{
+    res.json(user)
+})
+
+app.get("/user/:id",(req,res)=>{
+    res.send("user id is :- "+req.params.id)
+})
+
 
 app.listen(3000,(error)=>{
     if(error){
